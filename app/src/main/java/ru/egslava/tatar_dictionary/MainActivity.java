959d59e9e73df1722bffb33851234e1242473eae
Viacheslav Egorenkov <ve@poloniumarts.com>
2015-01-28 14:43:31 +0300

    tatar dictionary

---
+++ app/src/main/java/ru/egslava/tatar_dictionary/MainActivity.java
@@ -0,0 +1,93 @@
+package ru.egslava.tatar_dictionary;
+
+import android.app.ProgressDialog;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.FrameLayout;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.res.StringArrayRes;
+import org.androidannotations.annotations.res.StringRes;
+
+import ru.egslava.tatar_dictionary.R;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends ActionBarActivity {
+
+    @ViewById
+    PagerSlidingTabStrip    tabs;
+
+    @ViewById
+    FrameLayout ad;
+
+    @ViewById
+    AdView  adView;
+
+    @ViewById
+    ViewPager               pager;
+
+    DBHelper                db;
+    private ProgressDialog progressDialog;
+
+    public DBHelper db(){
+        if (db == null){
+            db = new DBHelper(this);
+        }
+        return db;
+    }
+
+    @StringArrayRes
+    String[] dicts;
+
+    @StringRes
+    String  please_wait;
+
+    @StringRes
+    String first_time_load;
+
+    @AfterViews
+    void init(){
+        progressDialog = ProgressDialog.show(this, please_wait, first_time_load, true, false);
+        adView.loadAd(new AdRequest.Builder().build());
+        loadDb();
+    }
+
+    @Background void loadDb(){ db().getReadableDatabase(); initPager();}
+
+    @UiThread(propagation = UiThread.Propagation.REUSE)
+    void initPager(){
+        pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override public int getCount() { return 2; }
+            @Override public Fragment getItem(int i) {
+                switch(i){
+                    case 0: return DictionaryFragment_.builder()
+                            .tableName("rus_tatar")
+                            .build();
+                    case 1: return DictionaryFragment_.builder()
+                            .tableName("tatar_rus")
+                            .letters(new String[]{"ә", "җ", "ң", "ө", "ү", "h"})
+                            .build();
+                }
+                return null;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return dicts[position];
+            }
+        });
+        tabs.setViewPager(pager);
+
+        progressDialog.cancel();
+    }
+}
---
+++ app/src/main/java/ru/egslava/tatar_dictionary/MainApplication.java
@@ -0,0 +1,38 @@
+package ru.egslava.tatar_dictionary;
+
+import android.app.Application;
+
+//import com.google.analytics.tracking.android.GoogleAnalytics;
+//import com.google.analytics.tracking.android.Tracker;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+import org.androidannotations.annotations.AfterInject;
+import org.androidannotations.annotations.EApplication;
+
+import ru.egslava.tatar_dictionary.R;
+
+/**
+ * Created by egslava on 20/12/14.
+ */
+
+@EApplication
+public class MainApplication extends Application {
+
+    Tracker tracker;
+
+    @AfterInject
+    void init(){
+        getTracker();
+//        tracker.send(new HitBuilders.ScreenViewBuilder().build());
+//        tracker.send(new HitBuilders.AppViewBuilder().build());
+    }
+    synchronized Tracker getTracker() {
+        if (tracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            tracker = analytics.newTracker(R.xml.app_tracker);
+        }
+        return tracker;
+    }
+}
