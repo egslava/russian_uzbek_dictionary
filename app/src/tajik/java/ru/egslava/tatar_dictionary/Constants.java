1dae4216360179484532971d05da6fff07d71b03
Viacheslav Egorenko <egslava@gmail.com>
2016-08-16 20:53:41 +0300

    Tajik app

---
+++ app/src/tajik/java/ru/egslava/tatar_dictionary/Constants.java
@@ -0,0 +1,10 @@
+package ru.egslava.tatar_dictionary;
+
+public class Constants {
+    public static final String source = "ru", target = "tj",
+
+    debugKey = "}|I~\u008E\u008FM\u0091\u0097\u0085Q\\^\\\\`^cdcg`fddeedlmlnjosvwv";
+
+
+    public static final String[] letters = {"ғ", "ӣ", "қ", "ӯ", "ҳ", "ҷ"};
+}
