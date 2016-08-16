69664fac25eec64e76797b749a676927cea5f9b7
Viacheslav Egorenko <egslava@gmail.com>
2016-08-16 22:18:57 +0300

    upd. Tajik keys

--- app/src/tajik/java/ru/egslava/tatar_dictionary/Constants.java
+++ app/src/tajik/java/ru/egslava/tatar_dictionary/Constants.java
@@ -3,7 +3,7 @@ package ru.egslava.tatar_dictionary;
 public class Constants {
     public static final String source = "ru", target = "tj",
 
-    debugKey = "}|I~\u008E\u008FM\u0091\u0097\u0085Q\\^\\\\`^cdcg`fddeedlmlnjosvwv";
+    debugKey = "}|I~\u008E\u008FM\u0091\u0097\u0085Q\\^\\\\`^cdcg`fddeedjkmnknppwo";
 
 
     public static final String[] letters = {"ғ", "ӣ", "қ", "ӯ", "ҳ", "ҷ"};
