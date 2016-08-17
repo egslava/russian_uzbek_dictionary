518516633e44b7f987f3bd6637fc1d0c35f92dcf
Viacheslav Egorenko <egslava@gmail.com>
2016-08-17 10:34:46 +0300

    [add]Kyrgyz app

---
+++ app/src/kirgiz/java/ru/egslava/tatar_dictionary/Constants.java
@@ -0,0 +1,10 @@
+package ru.egslava.tatar_dictionary;
+
+public class Constants {
+    public static final String source = "ru", target = "kg",
+
+    debugKey = "}|I~\u008E\u008FM\u0091\u0097\u0085Q\\^\\\\`^cdcg`fddeedimqjosmrwt";
+
+
+    public static final String[] letters = {"ө", "ң", "ү"};
+}
---
+++ app/src/uzbek/java/ru/egslava/tatar_dictionary/Constants.java
@@ -0,0 +1,9 @@
+package ru.egslava.tatar_dictionary;
+
+public class Constants {
+    public static final String source = "ru", target = "uz",
+
+    debugKey = "}|I~\u008E\u008FM\u0091\u0097\u0085Q\\^\\\\`^cdcg`fddeedolqqolppwq";
+
+    public static final String[] letters = {"ў", "қ", "ғ", "ҳ"};
+}
